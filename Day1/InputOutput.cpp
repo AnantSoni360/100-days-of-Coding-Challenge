0e35d5568c89a58cfaabb7f924da9fba38b92760
AnantSoni360 <147417339+AnantSoni360@users.noreply.github.com>
2025-10-31 23:31:21 +0530

    Add files via upload

---
+++ Day1/InputOutput.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, y, z;
+    cin >> x >> y >> z;    // Read three integers
+    cout << x + y + z << '\n';     // Print their sum
+    return 0;
+}
